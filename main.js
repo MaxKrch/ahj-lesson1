00097e929f1795fdcb495cb24b7dd2391941ea29
AppVeyor <ci@appveyor.com>
2023-12-27 17:04:12 +0000

    314fac2

---
+++ main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/app.js
+function demo(value) {
+  return `Demo: ${value}`;
+}
+console.log("app.js included");
+;// CONCATENATED MODULE: ./src/index.js
+
+
+/******/ })()
+;
